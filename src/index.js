3a5f9adffeae450ab91a2a4f79e57d5799c2b305
mstyslav <mstyslav2010@gmail.com>
2024-03-24 12:52:06 +0000

    add slider in customers section

--- src/index.js
+++ src/index.js
@@ -26,3 +26,25 @@
       bodyScrollLock.enableBodyScroll(document.body);
     });
   })();
+
+  // ============================================================
+  $(document).ready(function(){
+    $('.slider').slick({
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      appendDots: $(".slider"),
+      dotsClass: 'slick-dots'
+    });
+
+    $(window).on('resize', function() {
+      if ($(window).width() > 704) {
+        $('.slider').slick('slickSetOption', 'slidesToShow', 2, true);
+      } else {
+        $('.slider').slick('slickSetOption', 'slidesToShow', 1, true);
+      }
+    }).resize();
+  });
