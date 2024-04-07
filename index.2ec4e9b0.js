210eb57804ff1fdab5b42ba49632c24fc9c336e0
IvanKishko <134134175+IvanKishko@users.noreply.github.com>
2024-04-07 09:05:50 +0000

    Deploying to gh-pages from @ MstyslavSoroka/Yacht_Adventures@06f9b58fbad469521a9299219b4fc1cd0bb824af 🚀

---
+++ index.2ec4e9b0.js
@@ -0,0 +1,2 @@
+document.getElementById("myForm").addEventListener("submit",(function(e){e.preventDefault(),document.getElementById("popup").style.display="block"})),document.querySelector(".close").addEventListener("click",(function(e){e.preventDefault(),document.getElementById("popup").style.display="none"}));
+//# sourceMappingURL=index.2ec4e9b0.js.map
---
+++ index.932240fc.js
@@ -0,0 +1,2 @@
+document.getElementById("myForm").addEventListener("submit",(function(e){e.preventDefault(),document.getElementById("popup").style.display="block"})),document.querySelector(".close").addEventListener("click",(function(e){e.preventDefault(),document.getElementById("popup").style.display="none"}));
+//# sourceMappingURL=index.932240fc.js.map
